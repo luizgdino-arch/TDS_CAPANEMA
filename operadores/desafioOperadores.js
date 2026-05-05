ca4a747ea275a6048927f5efa40114faca598e13
luizgdino-arch <luizgdino@gmail.com>
2026-05-04 21:54:41 -0300

    Operadores em JS

---
+++ operadores/desafioOperadores.js
@@ -0,0 +1,13 @@
+//DESAFIO
+
+/*
+Atalho: SHIFT + ALT + A
+
+Utilizando os operadores relacionais, declare 2 variáveis e
+imprima no output/console 5 valores TRUE e 5 valores FALSE
+
+*/
+
+let irBemNaProva
+console.log 
+console.log
